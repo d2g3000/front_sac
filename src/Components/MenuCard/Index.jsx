291b03359a524058f828bad76ab9f5dd8f46c8ea
d2g3000 <d2g3000@gmail.com>
2023-05-31 12:31:03 -0400

    reparacion del overflow del menu

--- src/Components/MenuCard/Index.jsx
+++ src/Components/MenuCard/Index.jsx
@@ -4,16 +4,16 @@ const MenuCard = props => {
   const { id,title, imageUrl, modulo } = props
  
   return (
-    <div className="inline-block items-center mb-3 p-7">
+    <div className="inline-block items-center mb-3 ss:p-1 sm:p-7">
         <p className='text-xl text-black font-medium text-center mb-2'>{title}</p>
       <div className='flex items-center gap-2'>
-        <figure className='w-40 h-40'>
+        <figure className='ss:w-20 sm:w-40 ss:h-20 sm:h-40'>
           <img className='w-full h-full rounded-lg object-cover' src={imageUrl} alt={title} />
         </figure>
         
       </div>
      
-      <div className='flex items-center gap-2'>
+      <div className='flex items-center gap-1'>
         <p className='text-md '>Modulo: {modulo}</p>
     
       </div>
